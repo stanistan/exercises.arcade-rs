968fea7839d78861735b343b71b7b07d25a1ad29
Stan Rozenraukh <stan@stanistan.com>
2016-01-17 13:19:04 -0500

    w/ 3 second window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+extern crate sdl2;
+
+use sdl2::pixels::Color;
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+
+    // init
+    let sdl_context = sdl2::init().unwrap();
+    let video = sdl_context.video().unwrap();
+
+    // window
+    let window = video.window("ArcadeRS Shooter", 800, 600)
+        .position_centered().opengl()
+        .build().unwrap();
+
+    let mut renderer = window.renderer()
+        .accelerated()
+        .build().unwrap();
+
+    renderer.set_draw_color(Color::RGB(0, 0, 0));
+    renderer.clear();
+    renderer.present();
+
+    thread::sleep_ms(3000);
 }
